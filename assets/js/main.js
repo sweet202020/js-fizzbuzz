729b4f4c947eca484bad4bd9116ddd8ff29063a7
sweet202020 <dolcealessio1@gmail.com>
2022-10-10 14:54:38 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+alert("it works")
+/* Consegna: */
+//Scrivi un programma che stampi in console i numeri da 1 a 100
+
+// per i multipli di 3 stampi “Fizz” al posto del numero
+
+//per i multipli di 5 stampi “Buzz” al posto del numero.
+
+//Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
