ab51d8839f1508d99358cb61d68f05eef6e1c5c3
sweet202020 <dolcealessio1@gmail.com>
2022-10-10 15:20:40 +0200

    problem not defined 'i'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,24 @@
-alert("it works")
+
 /* Consegna: */
 //Scrivi un programma che stampi in console i numeri da 1 a 100
+for (let i=1; i<=100; i++ ){
+    console.log(i);
+}
+
+
+if (i % 3==0 ,i % 5==0){
+    console.log(i="fizzbuzz");
+} else if (i % 5 ==0 ){
+    console.log(i="buzz")
+} else if (i % 3==0 ){
+    console.log(i="fizz")
+}
+
+
 
 // per i multipli di 3 stampi “Fizz” al posto del numero
 
 //per i multipli di 5 stampi “Buzz” al posto del numero.
 
 //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
