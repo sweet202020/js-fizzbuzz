9a0c026350d81a00850dbfa60f00b5d10fa17871
sweet202020 <dolcealessio1@gmail.com>
2022-10-10 15:57:33 +0200

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 
 /* Consegna: */
 //Scrivi un programma che stampi in console i numeri da 1 a 100
+
 for (let i=1; i<=100; i++ ){
-    console.log(i);
+   
+    if (i % 3==0 && i % 5==0){
+        console.log("fizzbuzz");
+    } else if (i % 5 ==0 ){
+        console.log("buzz")
+    } else if (i % 3==0 ){
+        console.log("fizz")
+    } else{
+        console.log(i);
+    }
 }
 
-
-if (i % 3==0 ,i % 5==0){
-    console.log(i="fizzbuzz");
-} else if (i % 5 ==0 ){
-    console.log(i="buzz")
-} else if (i % 3==0 ){
-    console.log(i="fizz")
-}
 
 
 
